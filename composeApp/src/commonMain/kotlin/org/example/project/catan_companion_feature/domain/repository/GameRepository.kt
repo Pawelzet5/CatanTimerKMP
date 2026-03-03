b59363194e5f9c30826355bdbd4eb4a94fec829d
PawelZielinski <pawel.zielinski@bitapps.fi>
2026-03-03 16:00:23 +0100

    refactor: GameRepository no longer provides method getGames but fun getGameSummaries(): Flow<List<GameSummary>>

--- composeApp/src/commonMain/kotlin/org/example/project/catan_companion_feature/domain/repository/GameRepository.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/catan_companion_feature/domain/repository/GameRepository.kt
@@ -1,15 +1,13 @@
 package org.example.project.catan_companion_feature.domain.repository
 
-import org.example.project.catan_companion_feature.domain.dataclass.Game
-import org.example.project.catan_companion_feature.domain.dataclass.GameConfig
-import org.example.project.core.domain.DataError
-import org.example.project.core.domain.EmptyResult
-import org.example.project.core.domain.Result
+import kotlinx.coroutines.flow.Flow
+import org.example.project.catan_companion_feature.domain.dataclass.*
+import org.example.project.core.domain.*
 
 interface GameRepository {
-    suspend fun addGame(config: GameConfig): Result<Long, DataError.Local>
+    suspend fun addGame(config: GameConfig, startedAt: Long): Result<Long, DataError.Local>
     suspend fun getGame(gameId: Long): Result<Game, DataError.Local>
+    fun getGameSummaries(): Flow<List<GameSummary>>
     suspend fun getActiveGame(): Result<Game, DataError.Local>
-    suspend fun getGames(): Result<List<Game>, DataError.Local>
-    suspend fun saveGameAsFinished(gameId: Long): EmptyResult<DataError.Local>
+    suspend fun saveGameAsFinished(gameId: Long, finishedAt: Long): EmptyResult<DataError.Local>
 }
